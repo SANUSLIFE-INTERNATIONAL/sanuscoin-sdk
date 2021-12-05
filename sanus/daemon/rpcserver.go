ced1ca3b82210708225fb4ed54862d7fad63e431
YurGas <gasparyan.yur@gmail.com>
2021-12-05 20:17:12 +0300

    Fix orphan tx bug

--- sanus/daemon/rpcserver.go
+++ sanus/daemon/rpcserver.go
@@ -3322,7 +3322,7 @@ func handleSendRawTransaction(s *rpcServer, cmd interface{}, closeChan <-chan st
 
 	// Use 0 for the tag to represent local node.
 	tx := btcutil.NewTx(&msgTx)
-	acceptedTxs, err := s.cfg.TxMemPool.ProcessTransaction(tx, false, false, 0)
+	acceptedTxs, err := s.cfg.TxMemPool.ProcessTransaction(tx, true, false, 0)
 	if err != nil {
 		// When the error is a rule error, it means the transaction was
 		// simply rejected as opposed to something actually going wrong,
